e4f9a19d555dd87e19632dc5ae6f9aa78f765fb6
mmagnuski <mmagnuski@swps.edu.pl>
2016-04-29 23:54:11 +0200

    running files and understanding errors

---
+++ code/fix_errors.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+
+# radzenie sobie z błędami
+
+# powinno wyświetlić 23
+x = 25
+print(x - y)
+
+# powinno wyświetlić "muszę"
+tekst = "Zrobić krzywdę musze"
+słowa = tekst.split()
+print(słowa[3])
+
+# 
+wierszyk = słowa + "więc ładuję prędko kuszę"
+
+# 
+pth = r'C:\jakieś\dane\lecz\ich\brak'
+fls = os.listdir(pth)
+for f in fls:
+	print(f)
+
+#
+import mneeeee
